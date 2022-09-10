43880f7d12db88eb1e4c777bf251c64b54f6599e
Peter Saba <petersaba001@gmail.com>
2022-09-10 20:20:05 +0300

    added comments and onclick function

--- javascript/prediction.js
+++ javascript/prediction.js
@@ -1,9 +1,8 @@
 // automatically call the function to fetch a new dog image when the page is loaded/reloaded
 getRandomDog()
+// store all paragraph elements in the page to change them based on user input
 var paragraphs = document.getElementsByTagName("p")
-getGenderPrediction("peter")
-getAgePrediction("peter")
-getNationalityPrediction('peter')
+
 
 // fetch the random dog response from the api and insert the given url in the message attribute into the src attribute of the image tag
 async function getRandomDog(){
@@ -12,18 +11,21 @@ async function getRandomDog(){
     document.getElementById("dog_img").src = dogObject.message
 }
 
+// same as the getRandomDog function but this time adding the given data into the first paragraph of the page
 async function getGenderPrediction(name){
     let response = await fetch("https://api.genderize.io?name=" + name)
     let genderObject = await response.json()
     paragraphs[0].innerHTML = "Your gender is: <span>" + genderObject.gender + "</span>"
 }
 
+// same as the getGenderPrediction but for the 2nd paragraph
 async function getAgePrediction(name){
     let response = await fetch("https://api.agify.io/?name=" + name)
     let ageObject = await response.json()
     paragraphs[1].innerHTML = "Your age is: <span>" + ageObject.age + "</span>"
 }
 
+// same as the getGenderPrediction but this time checking the length to display nationalities based on the number of nationalities given
 async function getNationalityPrediction(name){
     let response = await fetch("https://api.nationalize.io/?name=" + name)
     let nationalityObject = await response.json()
@@ -35,3 +37,12 @@ async function getNationalityPrediction(name){
     }
 }
     
+// get the value eneterd by the user and pass it to the api related functions to retreive the necessary data
+async function predict(){
+    let name = document.getElementById("user_name").value
+    if (name){
+        await getGenderPrediction(name)
+        await getAgePrediction(name)
+        await getNationalityPrediction(name)
+    }
+}   
